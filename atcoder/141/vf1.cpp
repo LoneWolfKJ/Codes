7e0222ae80af93cd83c8d2ca3da1a0e962a868b0
Kanjal <kanjaldalal1000@gmail.com>
2019-09-30 21:14:48 +0530

    atcoder and kickstart19f

---
+++ atcoder/141/vf1.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+#define mp make_pair
+#define pb push_back
+
+ll power(ll a, ll n) {
+	ll p = 1;
+	while (n > 0) {
+		if(n%2) {
+			p = p * a;
+		} n >>= 1; a *= a;
+	} 
+	return p;
+}
+ll power(ll a, ll n, ll mod) {
+	ll p = 1;
+	while (n > 0) {
+		if(n%2) {
+			p = p * a; p %= mod;
+		} n >>= 1;
+	       	a *= a; a %= mod;
+	} 
+	return p % mod;
+}
+
+int main(){
+
+	return 0;
+}
+
