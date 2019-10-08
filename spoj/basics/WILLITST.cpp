d7966488fd774fe26519d01a94d292ed4f108dcc
Kanjal <kanjaldalal1000@gmail.com>
2019-10-08 16:44:48 +0530

    add spoj problems

---
+++ spoj/basics/WILLITST.cpp
@@ -0,0 +1,61 @@
+#include<bits/stdc++.h>
+using namespace std;
+using namespace std::chrono; 
+ 
+typedef long long int ll;
+#define mp make_pair
+#define pb push_back
+ 
+template <typename T>
+ostream& operator<< (ostream& out, const vector<T>& v) {
+   if ( !v.empty() ) {
+       copy (v.begin(), v.end(), ostream_iterator<T>(out, " "));
+       out << "\n";
+   }
+   return out;
+}
+ 
+ll power(ll a, ll n) {
+    ll p = 1;
+    while (n > 0) {
+       if(n%2) {
+          p = p * a;
+       } n >>= 1; a *= a;
+    }
+    return p;
+}
+ 
+ll powermodulo(ll a, ll n, ll mod) {
+ll p = 1;
+while (n > 0) {
+       if(n%2) {
+         p = p * a; p %= mod;
+       } n >>= 1;
+         a *= a; a %= mod;
+    } 
+    return p % mod;
+}
+ 
+int main(){
+ 
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    #ifndef ONLINE_JUDGE
+    auto start = high_resolution_clock::now(); 
+    freopen("in.txt", "rt", stdin);
+    freopen("out.txt", "wt", stdout);
+    #endif
+    //int t; cin >> t; while(t--){}
+    ll n;
+    cin >> n;
+    //cout << ((n&(n-1))==0) << "\n";
+    //cout << (n>1 && ((n&(n-1))==0)) << "\n";
+    cout << (((n&(n-1))==0)? "TAK\n" : "NIE\n");
+    #ifndef ONLINE_JUDGE
+    auto stop = high_resolution_clock::now(); 
+    auto duration = duration_cast<microseconds>(stop - start); 
+    cout << "Time Elapsed : " << duration.count() << " ";
+    #endif
+ 
+}
